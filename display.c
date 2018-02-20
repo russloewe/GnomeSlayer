95516a7d052d8cc909f26359ac72477276251abf
Russell Loewe <russloewe@gmail.com>
2018-02-20 15:39:33 -0800

    added formatting to load_image

--- display.c
+++ display.c
@@ -6,13 +6,26 @@ SDL_Surface * load_image(char * filename)
      * Takes a char pointer to the filename and returns pointer to
      * SDL_Surface containing loaded bitmap
      */
+    SDL_Surface* temp = NULL;
     SDL_Surface* image = NULL;
-    image = SDL_LoadBMP(filename);
-    if (image == NULL)
+    temp = SDL_LoadBMP(filename);
+    if (temp == NULL)
     {
         printf("ERROR: display.c -> load_image -> SDL_loadBMP(): %s\n", SDL_GetError());
         return NULL;
-    }else{
-        return image;
     }
+    //format the image
+    
+    SDL_SetColorKey(temp, (SDL_SRCCOLORKEY|SDL_RLEACCEL),
+                           SDL_MapRGB(temp->format, 0, 0, 0)); 
+    image = SDL_DisplayFormat(temp);
+    
+    if(image == NULL){
+        printf("ERROR: display.c -> load_image -> SDL_DisplayFormat(): %s\n", SDL_GetError());
+        return NULL;
+    }
+    
+    SDL_FreeSurface(temp);            
+
+    return image;
 }
