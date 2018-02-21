02e0e395d599b42a7987fb61ef8f5feada61e7a0
Russell Loewe <russloewe@gmail.com>
2018-02-20 19:16:05 -0800

    added tests

--- headers/test_suit.h
+++ headers/test_suit.h
@@ -7,12 +7,14 @@
 #include "SDL/SDL.h"
 #include "init.h"
 #include "input.h"
+#include "display.h"
+#include "gameobjects.h"
 
 int cleantest();
 int test1();
 int test2();
-
-int (*test_suit[])() = {test1, test2, cleantest};                   //cleantest must be at the end of this array
-const int test_len = sizeof(test_suit) / sizeof(test_suit[0]);      //global variable to tell test runner how many tests there are
+int test3();
+int test4();
+int test5();
 
 #endif
--- test_suit.c
+++ test_suit.c
@@ -9,8 +9,14 @@
  */
 #include "./headers/test_suit.h"
 
-int (*test_suit[])() = {test1, test2, cleantest};                   //cleantest must be at the end of this array
+int (*test_suit[])() = {test1, test2, test3, test4, test5, cleantest};                   //cleantest must be at the end of this array
 const int test_len = sizeof(test_suit) / sizeof(test_suit[0]);      //global variable to tell test runner how many tests there are
+
+int cleantest(){
+    printf("cleaning up tests\n");
+    cleanup();
+    return 0;
+}
 
 int test1(){
     printf("testing init.c->init_video() - ");
@@ -36,9 +42,87 @@ int test2(){
     return 1;
 }
 
-int cleantest(){
-    printf("cleaning up tests\n");
-    cleanup();
-    return 0;
+int test3(){
+    printf("Testing gameobjects.c -> createpiece() - ");
+    
+    gamepiece * player;
+    SDL_Surface * img;
+    
+    img = load_image("./img/player.bmp");
+    
+    player = create_piece(50, 50, img, PLAYER_TYPE);
+    
+    if( player->type != PLAYER_TYPE ){
+        printf("Failed 1\n");
+        return -1;
+    }else if( player->rect->x != 50 ){
+        printf("Failed 2\n");
+        return -1;
+    }else if(player->img == NULL){
+        printf("Failed 3\n");
+        return -1;
+    }else {
+        printf("Pass \n");
+        return 1;
+    }
+    
 }
 
+
+int test4(){
+    printf("Testing display.c->render_objects - ");
+    
+    gamepiece * player;
+    SDL_Surface * img;
+    
+    img = load_image("./img/player.bmp");
+    player = create_piece(50, 50, img, PLAYER_TYPE);
+
+    gamepiece * objects[] = {player};
+    
+    if(render_objects(objects) != 0){
+        printf("Failed\n");
+        return -1;
+    }else{
+        printf("Pass\n");
+        return 1;
+    }
+}
+
+int test5(){
+    printf("Testing gameobjects.c -> move_piece() - ");
+    
+    gamepiece * player;
+    SDL_Surface * img;
+    
+    img = load_image("./img/player.bmp");
+    player = create_piece(50, 50, img, PLAYER_TYPE);
+    
+    move_piece(player, MVUP);
+    move_piece(player, MVLEFT);
+    
+    if(player->rect->y != 49){
+        printf("Fail 1\n");
+        return -1;
+    }else if(player->rect->x != 49){
+        printf("Fail 2\n");
+        return -1;
+    }
+    
+    move_piece(player, MVDOWN);
+    move_piece(player, MVRIGHT);
+    move_piece(player, MVDOWN);
+    move_piece(player, MVRIGHT);
+    
+    if(player->rect->y != 51){
+        printf("Fail 1\n");
+        return -1;
+    }else if(player->rect->x != 51){
+        printf("Fail 2\n");
+        return -1;
+    }
+    
+    printf("Pass\n");
+    return 1;
+}
+    
