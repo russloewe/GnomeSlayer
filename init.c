da3826b7040e0dab719b1b94e49088aaa6bef3c5
Russell Loewe <russloewe@gmail.com>
2018-02-20 19:15:34 -0800

    added frame to screen and made screen surface global

--- init.c
+++ init.c
@@ -7,28 +7,30 @@
  */
 #include "./headers/init.h"
 
+SDL_Surface *screen;
+
 int init_video(void){
     
-    SDL_Surface *screen;  //declar pointer to SDL Surface
+      //declar pointer to SDL Surface
     
     /*
      * Initialize the SDL subsytem for video only. 
      * If initai
      */ 
     if( SDL_Init(SDL_INIT_VIDEO) != 0){
-        printf("ERROR: init.c -> init_video -> SDL_Init\n");
+        printf("ERROR: init.c -> init_video -> SDL_Init\n: %s\n", SDL_GetError());
         SDL_GetError();
         return 1;
     }
     
     /*
-     * Open a screen 640 by 480, no frame with the double buffer feature 
+     * Open a screen 640 by 480, with the double buffer feature 
      * to speed rendering.
      */
-    screen = SDL_SetVideoMode(640, 480, 0, SDL_HWPALETTE|SDL_NOFRAME|SDL_DOUBLEBUF);
+    screen = SDL_SetVideoMode(640, 480, 0, SDL_HWPALETTE|SDL_DOUBLEBUF);
     
     if(screen == NULL){
-        printf("ERROR: init.c -> init_video -> SDL_SetVideoMode \n");
+        printf("ERROR: init.c -> init_video -> SDL_SetVideoMode : %s\n", SDL_GetError());
         SDL_GetError();
         return 1;
     }
