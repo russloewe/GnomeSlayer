6e5951759ea6808fee1f61195d41d7b5bf812b5d
Russell Loewe <russloewe@gmail.com>
2018-02-20 19:17:05 -0800

    added player and movement to main loop

--- main.c
+++ main.c
@@ -10,20 +10,58 @@
 #include "SDL/SDL.h"
 #include "./headers/init.h"
 #include "./headers/input.h"
+#include "./headers/display.h"
 
 
 int main(void){
     
-       
+    SDL_Surface* image = NULL;   //image for player 
     
     init_video();      //open the screen
+
+    /*
+     * Create a player piece add it to array
+     */
+    gamepiece * player;
+    SDL_Surface * img;
+    img = load_image("./img/player2.bmp");
+    player = create_piece(50, 50, img, PLAYER_TYPE);
+    gamepiece * objects[] = {player};
     
+    //main loop 
     while(1){
-        
-        if( get_input() == 0 ){
-            cleanup();
-            return 0;
+        //parse input from sdl event, needs own function
+        switch(get_input()){
+            case 0:
+                cleanup();
+                return 0;
+                break;
+                
+            case 2:
+                move_piece(player, MVUP);
+                break;
+                
+            case 3:
+                move_piece(player, MVDOWN);
+                break;
+                
+            case 5:
+                move_piece(player, MVRIGHT);
+                break;
+                
+            case 4:
+                move_piece(player, MVLEFT);
+                break;
         }
+        
+        //render the objects to screen
+    
+        if(render_objects(objects) != 0){
+            printf("Failed\n");
+            return -1;
+        }
+        
+        //hold up a sec to not hog the cpu
         SDL_Delay(16);
     }
     
