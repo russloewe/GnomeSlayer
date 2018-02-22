f9890a71271d2e24dacb567628a447718783f0af
Russell Loewe <russloewe@gmail.com>
2018-02-22 01:26:33 -0800

    added handling for bad images

--- headers/display.h
+++ headers/display.h
@@ -8,6 +8,7 @@
 
 SDL_Texture * load_image(char * filename);
 int render_objects( gamepiece * pieces[]);
+SDL_Texture * make_colored_texture(int height, int width, Uint8 red, Uint8 blue, Uint8 green);
 int render_background();
 
 #endif
