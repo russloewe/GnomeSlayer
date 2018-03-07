72bd145d39295cd7e582318e65b4d136365f15ce
Russell Loewe <russloewe@gmail.com>
2018-03-07 00:25:22 -0800

    added example of new room render

--- main.c
+++ main.c
@@ -10,23 +10,40 @@
 #include "SDL2/SDL.h"
 #include "./headers/input.h"
 #include "./headers/graphics.h"
+#include "./headers/maps.h"
 
 
 int main(void){
     
-    
-    init_video();      //open the screen
+       init_video();      //open the screen
+        /***********temp room hack***************/
+        room room1 = {.walls = {NULL}}; //init walls array to null pointers
+        //make some walls
+        for(int i = 1; i < 11; i++){
+        gamepiece * wall = create_piece(5, i, load_image("./img/wall.bmp") , WALL_TYPE);
+        room1.walls[i-1] = wall;
+        }
+        //spawn some monsters
+        gamepiece * monst;
+        for(int i = 0; i<5; i++){
+        monst = create_piece(rand()%10+8, rand()%10+8, load_image("./img/player.bmp"), MONSTER_TYPE);
+        room1.monsters[i] =monst;
+    }
+    /******************end temp room hack**********/
+        
 
     /*
      * Create a player piece add it to array
      */
     gamepiece * player;
-    player = create_piece(5, 5, load_image("./img/wall.bmp"), PLAYER_TYPE);
+    player = create_piece(5, 5, load_image("./img/player.bmp"), PLAYER_TYPE);
     gamepiece * objects[10] = {NULL};
     objects[0] = player;
     
     //main loop 
     while(1){
+        
+
         //parse input from sdl event, needs own function
         switch(get_input()){
             case 0:
@@ -55,11 +72,16 @@ int main(void){
         render_background();
         
         //render the objects to screen    
-        if(render_objects(objects) != 0){
+        if(render_objects(objects,1) != 0){
             printf("Failed\n");
             return -1;
         }
-        
+            //test function 
+
+    
+        render_room(&room1);
+    
+    //end test function
         //hold up a sec to not hog the cpu
         SDL_Delay(16);
     }
