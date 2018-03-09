ce3b83966edf62bf316ec444d9443cc5e55933e7
Russell Loewe <russloewe@gmail.com>
2018-03-08 17:43:46 -0800

    use gamepiece with door type instead of defing new struct for doors

--- headers/maps.h
+++ headers/maps.h
@@ -4,21 +4,18 @@
 #include <stdio.h>
 #include "gameobjects.h"
 
+
 struct room{
     char name[50];
     gamepiece * monsters[5];
     gamepiece * bounty[5];
     gamepiece * walls[200];
-    struct door * lastroom;
-    struct door * nextroom;
+    gamepiece * doors[2];
 };
 
-struct door{
-    struct room * room1;
-    struct room * room2;
-};
+
 
 typedef struct room room;
-typedef struct door door;
+
 
 #endif
