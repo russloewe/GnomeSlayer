0979946127c6566a92b7f90e00034add09d52007
Russell Loewe <russloewe@gmail.com>
2018-03-10 22:06:15 -0800

    added space bar key bindings

--- headers/input.h
+++ headers/input.h
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include "gameobjects.h"
 #include "graphics.h"
+#include "checker.h"
+#include "maps.h"
 #include "SDL2/SDL.h"
 
 int get_input(void);
--- input.c
+++ input.c
@@ -5,7 +5,7 @@ int get_input(void){
     SDL_Event event;    //event handler or something
         
     while(SDL_PollEvent(&event)){
-
+            room * current_room = get_current_room();
             switch(event.type){
                 case SDL_QUIT:
                     return 0;
@@ -19,7 +19,7 @@ int get_input(void){
                             break;
                             
                         case SDLK_UP:
-                            move_piece(current_room->monsters[0], MVUP);
+                            move_piece(current_room->monsters[0], MVUP); //ned to replace with room interface to get player
                             add_message_queue("You Moved Up");
                             return 1;
                             break;
@@ -41,6 +41,28 @@ int get_input(void){
                             add_message_queue("You Moved Right");
                             return 1;
                             break;
+                            
+                        case SDLK_SPACE: ; //empty statement because of variable declared after label
+                            //attempt to grab item on ground
+                            gamepiece * item = player_on_item(get_current_room());
+                            
+                            if(item != NULL){ //check for item first
+                                piecetype type = get_piece_type(item); //grab the type
+                                
+                                if( (type == SWORD_TYPE) || (type == SHIELD_TYPE) ){ //for sword and shield
+                                    gamepiece * tmp_item = equip_item_to_player(current_room->monsters[0], item); //equip item and grab pointer to formeraly equipped item
+                                    destroy_piece(tmp_item);
+                                    
+                                    //construct and add message to queue
+                                    char message[40];                                    
+                                    strncat(message, "Equipped: ", 11);
+                                    strncat(message, get_piece_name(item), 20);
+                                    add_message_queue(message);
+                                }
+                            }
+                            return 1;
+                            break;
+                                
                     }
                 break;
             }
