2417df538f9a7ddfc8d586ae726e46ff9e499c38
Russell Loewe <russloewe@gmail.com>
2018-03-14 15:02:01 -0700

    still working

--- headers/draw.h
+++ headers/draw.h
@@ -1,1 +1,20 @@
+#ifndef _DRAW_H
+#define _DRAW_H
 
+int get_max_x();
+
+int get_max_y();
+int render_all();
+int load_images();
+int render_objects( gamepiece * pieces[], int range);
+int render_background_image(SDL_Texture * image);
+int render_room(room * cur_room);
+SDL_Rect get_char_rect(char c);
+Textline * get_message_queue();
+
+int add_message_queue(char * string);
+int trim_message_queue(int n);
+int render_message_queue(int line, int x, int y);
+int render_player_stats(room * curroom);
+int render_text_line(char * text, int x, int y);
+#endif
--- headers/graphics.h
+++ headers/graphics.h
@@ -5,18 +5,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <SDL2/SDL.h>
-#include "gameobjects.h"
-#include "maps.h"
-#include "checker.h"
 
 int init_video(void);
 void cleanup();
 int render_all();
-int render_text_line(char * text, int x, int y);
-int add_message_queue(char * string);
 int get_screen_height();
 int get_screen_width();
 int get_gamesquare();
-int get_max_x();
-int get_max_y();
+int render_object( Icon icon, int x, int y, int w, int h);
 #endif
